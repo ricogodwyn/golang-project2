ac2cd9ac82936fd2447813ab3fcb69da91ff847b
ricogod <125234686+ricogod@users.noreply.github.com>
2024-06-21 14:00:53 +0700

    first commit

---
+++ main.go
@@ -0,0 +1,94 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"math/rand"
+	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
+)
+
+type Movie struct{ //cuman template
+	ID string `json:"id"`
+	Isbn string `json:"isbn"`
+	Title string `json:"title"`
+	Director *Director `json:"director"`
+}
+
+type Director struct{
+	Firstname string `json:"firstname"`
+	Lastname string `json:"lastname"`
+}
+
+var movies []Movie
+
+func getMovies(w http.ResponseWriter,r *http.Request){
+	w.Header().Set("Content-Type","application/json") //set header
+	json.NewEncoder(w).Encode(movies)
+}
+
+func deleteMovie (w http.ResponseWriter,r *http.Request){
+	w.Header().Set("Content-Type","application/json") //set header
+	params:=mux.Vars(r)
+	for index, item := range movies {
+		if item.ID == params["id"]{
+			movies = append(movies[:index], movies[index+1:]...) //actually its not deleting but join before and after the index
+			break
+		}
+	}
+	json.NewEncoder(w).Encode(movies)
+}
+func getMovie(w http.ResponseWriter,r *http.Request){
+	w.Header().Set("Content-Type","application/json") //set header
+	params:=mux.Vars(r)
+	for _, item := range movies {
+		if item.ID == params["id"]{
+			json.NewEncoder(w).Encode(item)
+			return
+		}
+	}
+}
+
+func createMovie(w http.ResponseWriter,r *http.Request){
+	w.Header().Set("Content-Type","application/json") //set header
+	var movie Movie
+	_ = json.NewDecoder(r.Body).Decode(&movie)
+	movie.ID=strconv.Itoa(rand.Intn(1000000))
+	movies=append(movies,movie)
+	json.NewEncoder(w).Encode(movie)
+}
+
+func updateMovie(w http.ResponseWriter,r *http.Request){ //combine delete and create
+	w.Header().Set("Content-Type","application/json") //set header
+	params:=mux.Vars(r)
+	for index, item := range movies {
+		if item.ID == params["id"]{
+			movies = append(movies[:index], movies[index+1:]...) //actually its not deleting but join before and after the index
+			var movie Movie
+			_ = json.NewDecoder(r.Body).Decode(&movie)
+			movie.ID=params["id"]
+			movies=append(movies,movie)
+			json.NewEncoder(w).Encode(movie)
+			return
+		}
+	}
+
+}
+
+func main(){
+	r:=mux.NewRouter()
+
+	movies=append(movies,Movie{ID:"1",Isbn:"448743",Title:"Movie One",Director:&Director{Firstname:"John",Lastname:"Doe"}})
+	movies=append(movies,Movie{ID:"2",Isbn:"448744",Title:"Movie Two",Director:&Director{Firstname:"Steve",Lastname:"Smith"}})
+	r.HandleFunc("/movies",getMovies).Methods("GET")
+	r.HandleFunc("/movies/{id}",getMovie).Methods("GET")
+	r.HandleFunc("/movies",createMovie).Methods("POST")
+	r.HandleFunc("/movies/{id}",updateMovie).Methods("PUT")
+	r.HandleFunc("/movies/{id}",deleteMovie).Methods("DELETE")
+
+	fmt.Printf("starting at port 8080\n")
+	log.Fatal(http.ListenAndServe(":8080",r))
+}
